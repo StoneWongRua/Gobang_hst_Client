f0e76941b426a527e42fefa724347a21c7665b34
StoneWongRua <stonewongt@gmail.com>
2019-12-09 22:07:01 +0800

    first commit

---
+++ Assets/Gobang/Scripts/Multiplay/Network/Message.cs
@@ -0,0 +1,86 @@
+﻿
+using System;
+
+
+
+    public enum MessageType
+    {
+        None, //空
+        HeartBeat, //心跳
+        Enroll, //注册
+        CreateRoom, //创建房间
+        EnterRoom, //加入房间
+        ExitRoom, // 退出房间
+        StartGame, //开始游戏
+        PlayGame, //具体游戏开始
+    }
+
+    ///<summary>
+    /// 玩家类型
+    /// </summary>
+    public enum Chess
+    {
+        None,  // 空
+        Black, // 黑
+        White, // 白
+
+        Draw, //平局
+        Null, //表示无结果(如操作失败情况下的返回值)
+    }
+
+    [Serializable]
+    public class Enroll
+    {
+        public string username; //玩家姓名
+        public bool isSuc; //是否成功
+    }
+
+    [Serializable]
+    public class CreateRoom
+    {
+        public int roomId; //房间号码
+        public bool isSuc;   //是否成功
+    }
+
+    [Serializable]
+    public class EnterRoom
+    {
+        public int roomId;      //房间号码
+        public Result result;   //结果
+
+        public enum Result
+        {
+            None,
+            Player,
+            Observer,
+        }
+    }
+
+    [Serializable]
+    public class ExitRoom
+    {
+        public int roomId;  //房间号码
+        public bool isSuc;    //是否成功
+    }
+
+    [Serializable]
+    public class StartGame
+    {
+        public int roomId;            //房间号码
+
+        public bool isSuc;              //是否成功
+        public bool isFirst;            //是否先手
+        public bool isWatch;            //是否是观察者
+    }
+
+    [Serializable]
+    public class PlayGame
+    {
+        public int roomId;       //房间号码
+        public Chess Chess;      //棋子类型
+        public int X;            //棋子坐标
+        public int Y;            //棋子坐标
+
+        public bool isSuc;         //操作结果
+        public Chess Challenger; //胜利者
+    }
---
+++ Assets/Gobang/Scripts/Multiplay/Network/Tools/NetworkUtils.cs
@@ -0,0 +1,98 @@
+﻿using System.Net;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+using System.Reflection;
+
+public static class NetworkUtils
+{
+    /// <summary>
+    /// obj->bytes，如果obj未被标记成[serializable] 则返回null
+    /// 参考DirectProtoBufTools.Serialize(Object obj)
+    /// </summary>
+    public static byte[] Serialize(object obj)
+    {
+        //物体不为空且可被序列化
+        if (obj == null || !obj.GetType().IsSerializable)
+        {
+            return null;
+        }
+
+        IFormatter formatter = new BinaryFormatter();
+        formatter.Binder = new UBinder();
+        using (MemoryStream stream = new MemoryStream())
+        {
+            formatter.Serialize(stream, obj);
+            byte[] data = stream.ToArray();
+            return data;
+        }
+    }
+
+    /// <summary>
+    /// bytes->obj，如果obj未被标记成[serializable] 则返回null
+    /// 参考DirectProtoBufTools.Deserialize(Object obj)
+    /// </summary>
+    public static T Deserialize<T>(byte[] data) where T : class
+    {
+        //数据不为空且T是可序列化的类型
+        if (data == null || !typeof(T).IsSerializable)
+        {
+            return null;
+        }
+
+        IFormatter formatter = new BinaryFormatter();
+        formatter.Binder = new UBinder();
+        using (MemoryStream stream = new MemoryStream(data))
+        {
+            object obj = formatter.Deserialize(stream);
+            return obj as T;
+
+        }
+    }
+
+    /// <summary>
+    /// 获取本机IPv4,获取失败则返回null
+    /// </summary>
+    public static string GetLocalIPv4()
+    {
+        string hostname = Dns.GetHostName();
+        IPHostEntry ipEntry = Dns.GetHostEntry(hostname);
+        for (int i = 0; i < ipEntry.AddressList.Length; i++)
+        {
+            //从IP地址列表中筛选出IPv4类型的IP地址
+            if (ipEntry.AddressList[i].AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+            {
+                return ipEntry.AddressList[i].ToString();
+            }
+        }
+        return null;
+    }
+
+    /// <summary>
+    /// 比特数组 -> 字符串
+    /// </summary>
+    public static string Byte2String(byte[] bytes)
+    {
+        return Encoding.UTF8.GetString(bytes);
+    }
+
+    /// <summary>
+    /// 字符串 -> 比特数组
+    /// </summary>
+    public static byte[] String2Byte(string str)
+    {
+        return Encoding.UTF8.GetBytes(str);
+    }
+}
+
+public class UBinder : SerializationBinder
+{
+    public override Type BindToType(string assemblyName, string typeName)
+    {
+        Assembly ass = Assembly.GetExecutingAssembly();
+        return ass.GetType(typeName);
+    }
+}
